f1bc3360432fe171dc53f6b7bda141bc92cb3fc3
Jon Friis <jon@delphia.com>
2026-01-04 00:13:21 -0500

    Update homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-medium tracking-tight">Jon Friis</h1>
+      <h1 className="text-xl font-medium">Jon Friis, Designer</h1>
     </div>
   )
 }
