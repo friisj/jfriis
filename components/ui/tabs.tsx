7e8185b07a9deb8c90cb3cfa50e3d5a3c541b943
Jon Friis <jon@delphia.com>
2026-03-11 13:24:04 -0400

    refactor(luv): update TabsTrigger styling for improved layout
    - Enhanced the class names in TabsTrigger for better visual consistency and responsiveness.
    - Streamlined the styling by consolidating and adjusting class properties, ensuring a more cohesive design across tab components.

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -42,8 +42,13 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "dark:data-[state=active]:text-foreground border-b flex-1 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex items-center justify-start gap-1.5 text-xs uppercase tracking-wide h-10 font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:bg-accent focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-accent [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "border-b flex-1 inline-flex items-center justify-start gap-1.5 h-10 whitespace-nowrap",
+        "text-xs uppercase tracking-wide text-muted-foreground data-[state=active]:text-foreground font-medium dark:text-muted-foreground",
+        "transition-[color,box-shadow] [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         "px-3",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring focus-visible:bg-accent focus-visible:outline-1",
+        "data-[state=active]:bg-accent dark:data-[state=active]:text-foreground dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 ",
+        "disabled:pointer-events-none disabled:opacity-50",
         className
       )}
       {...props}
