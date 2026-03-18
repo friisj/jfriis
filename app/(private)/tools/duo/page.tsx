63a0a0b43515297ba5b3e773eedca8e81fc1ddd1
Jon Friis <jon@delphia.com>
2026-03-18 11:20:18 -0400

    Revert "refactor(duo): consolidate status bar into header"
    This reverts commit b41cfb304ab6664566a7018fbdea85c960e46dc7.

--- app/(private)/tools/duo/page.tsx
+++ app/(private)/tools/duo/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function DuoPage() {
   return (
-    <div className="h-full relative">
+    <div className="h-[calc(100vh-3rem)]">
       <DuoClient />
     </div>
   );
